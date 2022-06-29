00db39e098fafaa55adbba324fde2610a3029f4a
parth4apple <worldrecord00@gmail.com>
2022-06-28 23:12:30 -0700

    barebone map functionality

--- src/Restroom/RestroomMap.js
+++ src/Restroom/RestroomMap.js
@@ -1,12 +1,47 @@
 import React, {Component} from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-export default class RestroomMap extends Component {
-  render() {
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const ucsdCenter = {
+  lat: 32.8801,
+  lng: -117.2300
+};
+
+
+function MapComonent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyAGKGmhs8VyoDmXuRtsOzKSD0aZ43q0MPY"
+  })
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={ucsdCenter}
+        zoom={14}
+
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+const Map = React.memo(MapComonent)
+
+export default function RestroomMap() {
+  const redirectToLocation = () => {
+    console.log("hello")
+  }
     return(
       <>
-        <h2>Map.</h2>
-        <p> There will be a map here, centered at {this.props.latitude}, {this.props.longitude} </p>
+      <div style={{borderRadius:"15px", overflow: "hidden", zIndex:"5"}} onClick={() => redirectToLocation()}>
+        <Map />
+      </div>
       </>
     );
-  }
 }
