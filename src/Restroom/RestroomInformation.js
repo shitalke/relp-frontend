450cfe51e0bc45c27825b6336ae1b2b85559fc06
wes-chen <35469114+wes-chen@users.noreply.github.com>
2022-07-07 20:57:24 -0700

    add info section skeleton

--- src/Restroom/RestroomInformation.js
+++ src/Restroom/RestroomInformation.js
@@ -1,13 +1,35 @@
-import React, {Component} from 'react';
-import { Badge } from 'reactstrap';
+import React, {Component} from "react";
+import {Container, Col, Row} from "reactstrap";
+
+// components
+import Info from "../components/Info";
+
+// TODO: connect to the backend. Either pass in through props or make the call here.
+var fields = [
+  "3 stalls",
+  "wheelchair accessible",
+  "2 hand dryers",
+  "2 soap dispensers",
+  "3 sinks",
+  "mirror available"
+];
 
 export default class RestroomInformation extends Component {
   render() {
-    return(
-      <>
-        {this.props.handicap ? <Badge color="primary m-2" pill>Handicap</Badge>:<></>}
-        {this.props.single_use ? <Badge color="primary m-2" pill>Single Use</Badge>:<></>}
-      </>
+    // two columns, with hardcoded parameters for now.
+    // TODO: using <p> is a little hacky, change it to real styling later
+    return (
+      <Container>
+        <p />
+        <Row xs="2">
+          {fields.map(info => (
+            <Col>
+              <Info info={info} />
+            </Col>
+          ))}
+        </Row>
+        <p />
+      </Container>
     );
   }
 }
