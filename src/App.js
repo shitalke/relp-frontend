bf07cfca8546faa210b41c0902e6bfc5419db679
parth4apple <worldrecord00@gmail.com>
2022-07-20 11:04:09 -0700

    router set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LeaderboardPage from "./Leaderboard/LeaderboardPage";
+import RestroomPage from "./Restroom/RestroomPage";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <>
-      <LeaderboardPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="/restroom/:test" element={<RestroomPage />} />
+        <Route path="/restroom" element={<LeaderboardPage />} />
+      </Routes>
+    </Router>
   );
 }
 
