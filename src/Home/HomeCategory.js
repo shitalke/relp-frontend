642c13e1ac18c5ebe9967840c688cad421fcfde1
parth4apple <worldrecord00@gmail.com>
2022-08-01 18:51:18 -0700

    style fixes

--- src/Home/HomeCategory.js
+++ src/Home/HomeCategory.js
@@ -1,25 +1,22 @@
-import React, {useEffect} from "react";
+import React from "react";
 import styled from "styled-components";
 import RestroomCard from "../components/RestroomCard";
 
-function HomeCategory({restroomIds}) {
-
+function HomeCategory({ category, restroomIds }) {
   return (
-      <Content>
-          <CategoryTitle>Placeholder</CategoryTitle>
-          <RestroomRow>
-              {restroomIds && 
-                restroomIds.map(id =>  <RestroomCard id={id}/>)
-              }
-          </RestroomRow>
-      </Content>
-  )
+    <Content>
+      <CategoryTitle>{category}</CategoryTitle>
+      <RestroomRow>
+        {restroomIds && restroomIds.map((id) => <RestroomCard id={id} />)}
+      </RestroomRow>
+    </Content>
+  );
 }
 
 const Content = styled.div`
-display: flex;
-flex-direction: column;
-margin-bottom: 2.5vh;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2.5vh;
 `;
 
 const CategoryTitle = styled.div`
@@ -27,10 +24,9 @@ const CategoryTitle = styled.div`
 `;
 
 const RestroomRow = styled.div`
-display: flex;
-gap: 30px;
-overflow-x: auto;
-
+  display: flex;
+  gap: 30px;
+  overflow-x: auto;
 `;
 
 export default HomeCategory;
--- src/components/RestroomCard/index.js
+++ src/components/RestroomCard/index.js
@@ -1,20 +1,16 @@
-import React from 'react'
+import React from "react";
 import { Card } from "./style";
 import { useNavigate } from "react-router-dom";
 
-function RestroomCard({id}) {
-    let navigate = useNavigate();
+function RestroomCard({ id }) {
+  let navigate = useNavigate();
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-        navigate(`restroom/${id}`, { replace: true });
-    }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    navigate(`restroom/${id}`, { replace: true });
+  }
 
-    return (
-        <Card onClick={handleSubmit}>
-            card with restroom id: {id}
-        </Card>
-    )
+  return <Card onClick={handleSubmit}>card with restroom id: {id}</Card>;
 }
 
 export default RestroomCard;
