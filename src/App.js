f76b55f4a8a642bb3c9cc5074470b1814a02ac3f
parth4apple <worldrecord00@gmail.com>
2022-08-01 18:58:09 -0700

    routing change

--- src/App.js
+++ src/App.js
@@ -17,15 +17,13 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
 
-        <Route path="/leaderboard" element={<LeaderboardPage />} />
         <Route path="/leaderboard/:filter" element={<LeaderboardPage />} />
 
-        <Route path="/restroom" element={<RestroomPage />} />
         <Route path="/restroom/:restroom_id" element={<RestroomPage />} />
 
-        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profile/:user_id" element={<ProfilePage />} />
       
+        <Route path="*" element={<div>Oops, page not found</div>} />
       </Routes>
     </Router>
   );
